54341c1e0b0f8ee13dfcf672fdedbf4e9fc595a0
Jacq <jacq1212@gmail.com>
2014-10-29 21:56:20 +0100

    Added basic commands, dependencies (pixi.js). Testing build environment

--- test/core.tests.js
+++ test/core.tests.js
@@ -3,9 +3,23 @@
 "use strict";
 
 describe("Core tests", function () {
+    //debugger;
     it("Exist namespace", function () {
         expect(CrowdSim).not.toBeNull();
         expect(CrowdSim.World).toBeDefined();
+        expect(CrowdSim.Engine).toBeDefined();
     });
+
+    it("Engine", function () {
+        expect(CrowdSim.Engine).not.toBeNull();
+        expect(CrowdSim.Engine.running).toBe(false);
+        CrowdSim.Engine.run();
+        expect(CrowdSim.Engine.running).toBe(true);
+        CrowdSim.Engine.stop();
+        expect(CrowdSim.Engine.running).toBe(false);
+        CrowdSim.Engine.reset();
+        expect(CrowdSim.Engine.running).toBe(false);
+    });
+
 
 });
